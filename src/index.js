6748a5c86e607865389f5f48ecb1dedc6a1afc0b
Andrii <andrii.kondratovich@gmail.com>
2024-05-05 11:16:59 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g. 'api_key=live_FAKx2pDLsy0XqcD8zWm5HnJoTahsNP10pQpG021j9LSGUyM89HtdGFRCRrCvh7H1'
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] =
+  "live_FAKx2pDLsy0XqcD8zWm5HnJoTahsNP10pQpG021j9LSGUyM89HtdGFRCRrCvh7H1";
